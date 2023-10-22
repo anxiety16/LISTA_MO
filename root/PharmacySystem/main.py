a63f16804cd73cf60e4764c64bbcedf8fd5cfeae
Patrickgil11 <147314398+Patrickgil11@users.noreply.github.com>
2023-10-22 22:16:33 +0800

    Patrick (#2)
    * New UML diagram
    
    * Update Class: Pharmacist & Inventory
    
    * update

--- root/PharmacySystem/main.py
+++ root/PharmacySystem/main.py
@@ -9,13 +9,18 @@ class Customer:
 #Create class for the remaining (Pharmacist, Medication, Inventory, Prescription, & Order)
 
 class Pharmacist:
-
+    def __init__(self, PharmacistId, PharmacistName, LicenseNumber):
+        self.PharmacistId = PharmacistId
+        self.PharmacistName = PharmacistName
+        self.LicenseNumber = LicenseNumber
 
 class Medication:
 
 
 class Inventory:
-
+    def __init__(self,QuantityStock, MedicationId):
+        self.QuantityStock = QuantityStock
+        self.MedicationId = MedicationId
 
 class Prescription:
 
