ac67797cf7ec8dd0c73f5942b2e59aea53a0bb45
Marvin Joseph Cajilo <202040175@psu.palawan.edu.ph>
2023-10-23 01:47:24 +0800

    working user input, still buggy on end

--- root/PharmacySystem/main.py
+++ root/PharmacySystem/main.py
@@ -14,15 +14,18 @@ class Pharmacist:
         self.PharmacistName = PharmacistName
         self.LicenseNumber = LicenseNumber
 
+    def loggin(self):
+        print(f'\nWelcome {self.PharmacistName}')
+
 class Medication:
-    def __init__(self, MedicationId, MedicationName, Description, Dosage, Price, Manufacturer, ExpirationDate):
+    def __init__(self, MedicationId, MedicationName, Dosage, Price):
         self.MedicationId = MedicationId
         self.MedicationName = MedicationName
-        self.Description = Description
         self.Dosage = Dosage
         self.Price = Price
-        self.Manufacturer = Manufacturer
-        self.ExpirationDate = ExpirationDate 
+
+    def GetPrice(self):
+        return self.Price
 
 class Inventory:
     def __init__(self,QuantityStock, MedicationId):
@@ -37,8 +40,53 @@ class Prescription:
         self.DateIssued = DateIssued
 
 class Order:
-    def __init__(self, OrderId, Customer, Medication, TotalPrice):
+    def __init__(self, OrderId, Customer, Medications):
         self.OrderId = OrderId
         self.Customer = Customer
-        self.Medication = Medication
-        self.TotalPrice = TotalPrice
+        self.Medications = Medications
+        self.TotalPrice = sum(Medication.GetPrice() for Medication in Medications)
+
+#FOR LOGGING IN OF PHARMACIST ADMIN
+user = input("Enter Pharmacist License Number: ")
+if user == '0101':
+    customers = []
+    medications = []
+
+    customer = int(input("Enter the number of customers: "))
+
+    for i in range(customer):
+        name = input("Customer Name: ")
+        address = input("Customer Address: ")
+        phone = input("Customer Contact Number: ")
+        customers.append(Customer(len(customers) + 1, name, address, phone))
+
+    meds = int(input("Enter the number of medications: "))
+    for i in range(meds):
+        name = input("Medication Name: ")
+        dosage = int(input("Dosage: "))
+        price = float(input("Medication Price: "))
+        medications.append(Medication(len(medications) + 1, name, dosage, price))
+
+    CustomerId = int(input("Enter customer ID for the prescription: "))
+    MedicationId = int(input("Enter medication ID for the prescription: "))
+    DateIssued = input("Enter prescription date: ")
+    customer = next((c for c in customers if c.CustomerId == CustomerId), None)
+    medication = next((m for m in medications if m.MedicationId == MedicationId), None)
+
+    if customer and medication:
+        prescription = Prescription(len(customer.Prescription) + 1, customer, medication, DateIssued)
+        customer.Prescription.append(Prescription)
+        print("Prescription created.")
+    else:
+        print("Invalid customer or medication ID.")
+
+# Display prescriptions for a customer
+    CustomerId = int(input("Enter customer ID to display prescriptions: "))
+    customer = next((c for c in customers if c.CustomerId == CustomerId), None)
+
+    if customer:
+        print(f"Prescriptions for {customer.CustomerName}:")
+        for Prescription in customer.Prescription:
+            print(f"Prescription ID: {Prescription.PrescriptionId}, Medication: {Prescription.Medication.MedicationName}, Date: {Prescription.DateIssued}")
+    else:
+        print("Invalid customer ID.")
